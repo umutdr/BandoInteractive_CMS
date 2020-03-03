6eb2787207476edb97ac8ea502360666dd24531b
Umut <umutdr98@gmail.com>
2020-03-03 11:47:43 +0300

    Entity Layer Created Entities Added

---
+++ BandoInteractive_CMS.Entity/Entities/Layout.cs
@@ -0,0 +1,11 @@
+﻿namespace BandoInteractive_CMS.Entity.Entities
+{
+    public class Layout
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+    }
+}
---
+++ BandoInteractive_CMS.Entity/Entities/Page.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace BandoInteractive_CMS.Entity.Entities
+{
+    public class Page
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public string URL { get; set; }
+
+        public int LayoutId { get; set; }
+
+        public Layout Layout { get; set; }
+
+        public int ParentPageId { get; set; }
+
+        public Page ParentPage { get; set; }
+
+        public IQueryable<Page> ChildPages { get; set; }
+    }
+
+}
